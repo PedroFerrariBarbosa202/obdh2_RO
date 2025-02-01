b985df3db7923e77097d3d25ee71e183f0caa6c6
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-02-01 13:04:01 -0300

    firmware: app: tasks: process_tc: Add sensor reading ts to OBDH data format

--- firmware/app/tasks/process_tc.c
+++ firmware/app/tasks/process_tc.c
@@ -1861,8 +1861,12 @@ static int8_t format_data_request(uint8_t *pkt_pl, uint16_t *pkt_pl_len, uint8_t
 			pl[74] = ((uint16_t)tel->data.position.longitude) & 0xFFU;
 			pl[75] = (((uint16_t)tel->data.position.altitude) >> 8U) & 0xFFU;
 			pl[76] = ((uint16_t)tel->data.position.altitude) & 0xFFU;
+			pl[77] = (tel->data.ts_read_sensors >> 24U) & 0xFFU;
+			pl[78] = (tel->data.ts_read_sensors >> 16U) & 0xFFU;
+			pl[79] = (tel->data.ts_read_sensors >> 8U) & 0xFFU;
+			pl[80] = tel->data.ts_read_sensors & 0xFFU;
 
-			*pkt_pl_len = (uint16_t) 86U; /* 7b RQ CALLSIGN + 1b TC ID + 77b OBDH DATA */
+			*pkt_pl_len = (uint16_t) 89U; /* 7b RQ CALLSIGN + 1b TC ID + 81b OBDH DATA */
 
 			break;
 		}
