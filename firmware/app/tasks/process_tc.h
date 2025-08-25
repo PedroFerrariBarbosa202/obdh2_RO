80c05c660b571899f8bd5bb770526e4982193653
Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
2025-08-25 16:53:09 -0300

    firmware: app: tasks: process_tc: Increase task stack size to accomodate `Data Request`

--- firmware/app/tasks/process_tc.h
+++ firmware/app/tasks/process_tc.h
@@ -1,35 +1,35 @@
 /*
  * process_tc.h
- * 
+ *
  * Copyright The OBDH 2.0 Contributors.
- * 
+ *
  * This file is part of OBDH 2.0.
- * 
+ *
  * OBDH 2.0 is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * OBDH 2.0 is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with OBDH 2.0. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 /**
  * \brief Process TC task definition.
- * 
+ *
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * \author Carlos Augusto Porto Freitas <carlos.portof@hotmail.com>
- * 
+ *
  * \version 1.0.0
- * 
+ *
  * \date 2021/07/06
- * 
+ *
  * \defgroup process_tc Process TC
  * \ingroup tasks
  * \{
@@ -44,7 +44,7 @@
 #include <task.h>
 
 #define TASK_PROCESS_TC_NAME                "Process TC"        /**< Task name. */
-#define TASK_PROCESS_TC_STACK_SIZE          1024                /**< Stack size in bytes. */
+#define TASK_PROCESS_TC_STACK_SIZE          2048                /**< Stack size in bytes. */
 #define TASK_PROCESS_TC_PRIORITY            4                   /**< Task priority. */
 #define TASK_PROCESS_TC_PERIOD_MS           1000                /**< Task period in milliseconds. */
 #define TASK_PROCESS_TC_INITIAL_DELAY_MS    6500                /**< Delay, in milliseconds, before the first execution. */
@@ -74,7 +74,7 @@ void vTaskProcessTC(void* p);
  *
  * \param[in] is_scheduled is a flag to mark if a telecommand packet
  * was scheduled instead of just received. This is needed because in a
- * scheduled TC there is no need to authenticate it, since it was 
+ * scheduled TC there is no need to authenticate it, since it was
  * authenticated on queue insertion.
  *
  * \return The status/error code.
