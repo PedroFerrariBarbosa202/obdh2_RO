ad8837e8d0cc407720e25075f0ce3c93c546f75f
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2026-05-21 14:01:22 -0300

    Preparing for release v1.0

--- firmware/version.h
+++ firmware/version.h
@@ -25,7 +25,7 @@
  * 
  * \author Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
  * 
- * \version 0.10.20
+ * \version 1.0.0
  * 
  * \date 2019/10/25
  * 
@@ -38,7 +38,7 @@
 
 #define FIRMWARE_VERSION            "1.0.0"
 
-#define FIRMWARE_STATUS             "Development"
+#define FIRMWARE_STATUS             "Production"
 
 #define FIRMWARE_AUTHOR             "SpaceLab-UFSC"
 
