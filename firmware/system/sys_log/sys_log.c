446d6f1a0cc08b6e6346aea46ecce660125ec886
Pedro Ferrari Barbosa <pedro.ferraribarbosa2007@gmail.com>
2026-05-26 17:23:28 -0300

    fixed hex representation error

--- firmware/system/sys_log/sys_log.c
+++ firmware/system/sys_log/sys_log.c
@@ -315,10 +315,10 @@ void sys_log_print_bit_array_as_hex(uint64_t bit_array, uint16_t bytes_to_read)
     {
         uint8_t curr_byte = 0;
 
-        for (uint16_t i = 0; i < 8; i++)
+        for (int i = 7; i >= 0; i--)
         {
             uint8_t curr_bit = (bit_array >> (i + offset)) & 1;
-            curr_byte |= (curr_bit << i);
+            curr_byte |= (curr_bit << 7 - i);
         }
 
         sys_log_print_hex(curr_byte);
@@ -326,8 +326,6 @@ void sys_log_print_bit_array_as_hex(uint64_t bit_array, uint16_t bytes_to_read)
 
         offset += 8;
     }
-
-    sys_log_print_msg("\n");
 }
 
 
